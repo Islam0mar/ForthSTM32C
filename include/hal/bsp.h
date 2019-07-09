e416d47dc90bdce29af2d5ee8cfcdbb8383c32f9
CatchMeFastFat <io1131@fayoum.edu.eg>
2019-07-09 03:21:03 +0200

    Fix UART

--- include/hal/bsp.h
+++ include/hal/bsp.h
@@ -12,13 +12,19 @@
 #include <stdint.h>
 #include <string.h>
 
-#include "global.h"
+#include "forth/global.h"
+#include "system_stm32f1xx.h"
 
 #define HSE_VALUE 8000000U
 #define HSI_VALUE 8000000U
 /*!< Vector Table base offset field. This value must be a multiple of 0x200. */
 #define VECT_TAB_OFFSET 0x00000000U
 /* Should be divisable by 2 (see HAL_UART_RxCpltCallback)*/
+#define UART1_FIFO_SIZE
+
+extern uint32_t SystemCoreClock;          /*!< System Clock Frequency (Core Clock) */
+extern const uint8_t  AHBPrescTable[16];  /*!< AHB prescalers table values */
+extern const uint8_t  APBPrescTable[8];   /*!< APB prescalers table values */
 
 // #include "stm32f1xx_hal_conf.h"
 // #include "stm32f1xx_hal_uart.h"
@@ -34,5 +40,7 @@ void UART1_Init();
 void GPIO_Init();
 void Init();
 void UART1_Send();
+extern void SystemInit(void);
+extern void SystemCoreClockUpdate(void);
 
 #endif /* BSP_H */
